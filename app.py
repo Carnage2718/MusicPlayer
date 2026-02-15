8f3a48cbc9574be26dcf7b3577ab778d08178230
Carnage2718 <carnage271828@gmail.com>
2026-02-15 20:19:39 +0900

    app.py ud

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ def home():
 if __name__ == "__main__":
     app.run()
     
-import os import psycopg2
-@app.route("dbtest")
+import os 
+import psycopg2
+@app.route("/dbtest")
 def dbtest():
     conn = psycopg2.connect(os.environ["DATABASE_URL"])
     cur.execute("SELECT 1;")
