2103446214b5c7b15d1381a2130aa282574f6733
Carnage2718 <carnage271828@gmail.com>
2026-02-15 20:10:39 +0900

    app.pyの更新

--- app.py
+++ app.py
@@ -8,3 +8,12 @@ def home():
 if __name__ == "__main__":
     app.run()
     
+import os import psycopg2
+@app.route("dbtest")
+def dbtest():
+    conn = psycopg2.connect(os.environ["DATABASE_URL"])
+    cur.execute("SELECT 1;")
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    return f"DB Connected: {result}"
