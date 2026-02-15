76ce5eb17ba4932868b5df74310ac2eb77d62074
Carnage2718 <carnage271828@gmail.com>
2026-02-15 21:11:46 +0900

    app.py up

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
@@ -8,14 +10,62 @@ def home():
 if __name__ == "__main__":
     app.run()
     
-import os 
-import psycopg2
-@app.route("/dbtest")
-def dbtest():
+@app.route("/initdb")
+def initdb():
     conn = psycopg2.connect(os.environ["DATABASE_URL"])
     cur = conn.cursor()
-    cur.execute("SELECT 1;")
-    result = cur.fetchone()
+
+    #songs
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS songs (
+        id SERIAL PRIMARY KEY,
+        title TEXT NOT NULL,
+        release_year INTEGER,
+        stream_url TEXT NOT NULL,
+        original_url TEXT,
+        cover_url TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    );
+    """)
+
+    #artists
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS artists (
+        id SERIAL PRIMARY KEY,
+        name TEXT UNIQUE NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    """)
+
+    #song_artists
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS song_artists (
+        id SERIAL PRIMARY KEY,
+        song_id INTEGER REFERENCES songs(id) ON DELETE CASCADE,
+        artist_id INTEGER REFERENCES artists(id) ON DELETE CASCADE
+        role TEXT NOT NULL CHECK (role IN ('main', 'featured'))
+    );
+    """)
+
+    #favorite_types
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS favorite_types (
+        id SERIAL PRIMARY KEY,
+        name TEXT UNIQUE NOT NULL
+    );
+    """)
+
+    #song_favorites
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS song_favorites (
+        id SERIAL PRIMARY KEY,
+        song_id INTEGER REFERENCES songs(id) ON DELETE CASCADE,
+        favorite_type_id INTEGER REFERENCES favorite_types(id)
+    );
+    """)
+
+    conn.commit()
     cur.close()
     conn.close()
-    return f"DB Connected: {result}"
+
+    return "Database Initialized Successfully!"
