30a60547fca5642b0beee61d3bc564893e4091de
Carnage2718 <carnage271828@gmail.com>
2026-04-22 11:29:15 +0900

    add uvicorn

--- music-ui/music-ui/src/api.js
+++ music-ui/music-ui/src/api.js
@@ -5,6 +5,9 @@
 //online用　
 // const API_BASE = "http://192.168.3.34:8000"
 
-const API_BASE = "http://192.168.3.34:8000"
+//Render用
+//const API_BASE = import.meta.env.VITE_API_BASE
+
+const API_BASE = "http://127.0.0.1:8000"
 
 export default API_BASE
